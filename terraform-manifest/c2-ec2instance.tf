cc51d931062908988c6dca0fa9f5c8e476aca7af
abhilash0326 <112879718+abhilash0326@users.noreply.github.com>
2022-11-24 17:39:54 +0530

    Update c2-ec2instance.tf
    aa

--- terraform-manifest/c2-ec2instance.tf
+++ terraform-manifest/c2-ec2instance.tf
@@ -1,4 +1,8 @@
 resource "aws_instance" "myVM" {
     ami = "ami-0beaa649c482330f7"
     instance_type = "t2.micro"
+      user_data = file("${path.module}/app1-install.sh")
+tags = {
+    Name = "my Demo vm"
+}
 }
